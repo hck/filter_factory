7a012884bcf61074acf16fa6d6bcdb02f7e1f778
hck <alexsvirin@gmail.com>
2016-04-09 23:50:33 +0200

    Use RSpec 3 syntax in field and filter specs.

--- spec/filter_factory/field_spec.rb
+++ spec/filter_factory/field_spec.rb
@@ -1,60 +1,58 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe FilterFactory::Field do
-  describe "#initialize" do
-    it "should create field with name & condition specified" do
+RSpec.describe FilterFactory::Field do
+  describe '#initialize' do
+    it 'creates field with name & condition specified' do
       field = described_class.new(:name, :eq)
-      field.should be_instance_of(described_class)
-      field.alias.should == :name
-      field.name.should == :name
-      field.condition.should == :eq
+      expect([field.alias, field.name, field.condition]).to eq([:name, :name, :eq])
     end
 
-    it "should create field with name, condition & options specified" do
+    it 'creates field with name, condition & options specified' do
       field = described_class.new(:name, :eq, alias: :my_name)
-      field.should be_instance_of(described_class)
-      field.alias.should == :my_name
-      field.name.should == :name
-      field.condition.should == :eq
+      expect([field.alias, field.name, field.condition]).to eq([:my_name, :name, :eq])
     end
 
-    it "should raise error if name is not specified" do
-      expect{ described_class.new }.to raise_error(ArgumentError)
+    it 'raises error if name is not specified' do
+      expect { described_class.new }.to raise_error(ArgumentError)
     end
 
-    it "should raise error if condition is not specified" do
-      expect{ described_class.new(:name) }.to raise_error(ArgumentError)
+    it 'raises error if condition is not specified' do
+      expect { described_class.new(:name) }.to raise_error(ArgumentError)
     end
 
-    it "should raise error if wrong condition specified" do
-      expect{ described_class.new(:name, :my_eq) }.to raise_error(ArgumentError)
+    it 'raises error if wrong condition specified' do
+      expect { described_class.new(:name, :my_eq) }.to raise_error(ArgumentError)
     end
   end
 
-  describe "#==" do
-    it "should be equal to obj if it is an instance of Field & it has equal name, condition & alias" do
+  describe '#==' do
+    it 'equals to obj if it is an instance of Field & it has equal name, condition & alias' do
       f1 = described_class.new(:name, :eq)
       f2 = described_class.new(:name, :eq, alias: :name)
-      f1.should == f2
+      expect(f1 == f2).to eq(true)
     end
 
-    it "should not be equal to obj if it is not an instance of Field" do
+    it 'is not equal to obj if it is not an instance of Field' do
       f = described_class.new(:name, :eq)
-      f.should_not == []
-      f.should_not == {}
-      f.should_not == 0
+      expect([f == [], f == {}, f == 0]).to eq([false, false, false])
     end
 
-    it "should not be equal to obj if it is an instance of Field & it does not have equal name" do
-      described_class.new(:name, :eq).should_not == described_class.new(:my_name, :eq)
+    it 'is not equal to obj if it is an instance of Field & it does not have equal name' do
+      f1 = described_class.new(:name, :eq)
+      f2 = described_class.new(:my_name, :eq)
+      expect(f1 == f2).to eq(false)
     end
 
-    it "should not be equal to obj if it is an instance of Field & it does not have equal condition" do
-      described_class.new(:name, :eq).should_not == described_class.new(:name, :ne)
+    it 'is not equal to obj if it is an instance of Field & it does not have equal condition' do
+      f1 = described_class.new(:name, :eq)
+      f2 = described_class.new(:name, :ne)
+      expect(f1 == f2).to eq(false)
     end
 
-    it "should not be equal to obj if it is an instance of Field & it does not have equal alias" do
-      described_class.new(:name, :eq).should_not == described_class.new(:name, :eq, alias: :my_name)
+    it 'is not equal to obj if it is an instance of Field & it does not have equal alias' do
+      f1 = described_class.new(:name, :eq)
+      f2 = described_class.new(:name, :eq, alias: :my_name)
+      expect(f1 == f2).to eq(false)
     end
   end
 end
--- spec/filter_factory/filter_spec.rb
+++ spec/filter_factory/filter_spec.rb
@@ -1,8 +1,8 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe FilterFactory::Filter do
-  describe "::create" do
-    it "should create filter and execute block" do
+RSpec.describe FilterFactory::Filter do
+  describe '::create' do
+    it 'creates filter and execute block' do
       test_fields = [[:field1, :eq], [:field2, :eq], [:field3, :eq]]
 
       filter = described_class.create do
@@ -11,115 +11,118 @@ describe FilterFactory::Filter do
         end
       end
 
-      filter.should be_a(described_class)
-
-      filter.fields.map{|f| [f.name, f.condition]}.should == test_fields
+      expect(filter.fields.map {|f| [f.name, f.condition]}).to eq(test_fields)
     end
 
-    it "should define singleton method for defined field by its name if no alias option specified" do
+    it 'defines singleton method for defined field by its name if no alias option specified' do
       filter = described_class.create do
         field :name, :eq
       end
 
-      filter.should respond_to(:name, :'name=')
+      expect(filter).to respond_to(:name, :'name=')
     end
 
-    it "should define singleton method for defined field by its alias if alias option specified" do
+    it 'defines singleton method for defined field by its alias if alias option specified' do
       filter = described_class.create do
         field :name, :eq, alias: :my_name
       end
 
-      filter.should respond_to(:my_name, :'my_name=')
+      expect(filter).to respond_to(:my_name, :'my_name=')
     end
 
-    it "should raise error if duplicate field definition found" do
-      expect do
+    it 'raises error if duplicate field definition found' do
+      expect {
         described_class.create do
           field :name, :eq
           field :surname, :regex, alias: :last_name
           field :name, :eq, alias: :name
         end
-      end.to raise_error(FilterFactory::Filter::DuplicateFieldError)
+      }.to raise_error(FilterFactory::Filter::DuplicateFieldError)
     end
   end
 
-  describe "#<field_name>, #<field_name>=" do
-    it "should get field value" do
+  describe '#<field_name>, #<field_name>=' do
+    it 'gets field value' do
       filter = described_class.create do
         field :name, :eq
       end
 
-      filter.fields.first.value = "sample name"
-      filter.name.should == "sample name"
+      filter.fields.first.value = 'sample name'
+      expect(filter.name).to eq('sample name')
     end
 
-    it "should set field value" do
+    it 'sets field value' do
       filter = described_class.create do
         field :name, :eq
       end
 
-      filter.name = "sample name"
-      filter.fields.first.value.should == "sample name"
+      filter.name = 'sample name'
+      expect(filter.fields.first.value).to eq('sample name')
     end
   end
 
-  describe "#attributes, #attributes=" do
-    it "should respond_to attributes & attributes= methods" do
+  describe '#attributes, #attributes=' do
+    it 'respond_tos attributes & attributes= methods' do
       filter = described_class.create do
         field :name, :eq
       end
 
-      filter.should respond_to(:attributes, :attributes=)
+      expect(filter).to respond_to(:attributes, :attributes=)
     end
 
-    it "should return valid attributes" do
+    it 'returns valid attributes' do
       filter = described_class.create do
         field :name, :eq
         field :surname, :regex, alias: :last_name
       end
-      filter.name = "test name"
+      filter.name = 'test name'
 
-      filter.attributes.should == HashWithIndifferentAccess.new({name: "test name", last_name: nil})
+      expected_attributes = HashWithIndifferentAccess.new(name: 'test name', last_name: nil)
+
+      expect(filter.attributes).to eq(expected_attributes)
     end
 
-    it "should fill filter values from hash" do
+    it 'fills filter values from hash' do
       filter = described_class.create do
         field :name, :eq
         field :surname, :regex, alias: :last_name
       end
 
-      attributes = {name: "my test name", last_name: "surname here"}
+      attributes = { name: 'my test name', last_name: 'surname here' }
       filter.attributes = attributes
-      filter.attributes.should == HashWithIndifferentAccess.new(attributes)
+      expect(filter.attributes).to eq(HashWithIndifferentAccess.new(attributes))
     end
   end
 
-  describe "#get_field" do
-    it "should respond to #get_field method" do
-      should respond_to(:get_field)
+  describe '#get_field' do
+    it 'responds to #get_field method' do
+      expect(subject).to respond_to(:get_field)
     end
 
-    it "should return valid field by calling #get_field" do
+    it 'returns valid field by calling #get_field' do
       filter = described_class.create do
         field :name, :eq
         field :surname, :regex, alias: :last_name
       end
-      filter.get_field(:name).should be_instance_of(FilterFactory::Field)
-      filter.get_field(:name).condition.should == :eq
-      filter.get_field(:surname).should be_instance_of(FilterFactory::Field)
-      filter.get_field(:surname).condition.should == :regex
-      filter.get_field(:my_name).should be_nil
+
+      field_name = filter.get_field(:name)
+      expect([field_name.class, field_name.condition]).to eq([FilterFactory::Field, :eq])
+
+      field_surname = filter.get_field(:surname)
+      expect([field_surname.class, field_surname.condition]).to eq([FilterFactory::Field, :regex])
+
+      expect(filter.get_field(:my_name)).to be_nil
     end
   end
 
   described_class::CONDITIONS.each do |condition|
-    it "should respond to #{condition} method" do
-      should respond_to(condition)
+    it 'responds to #{condition} method' do
+      expect(subject).to respond_to(condition)
     end
 
-    it "should define field with '#{condition}' condition" do
+    it "defines field with '#{condition}' condition" do
       filter = described_class.create{ public_send(condition, :name) }
-      filter.get_field(:name).condition.should == condition
+      expect(filter.get_field(:name).condition).to eq(condition)
     end
   end
 end

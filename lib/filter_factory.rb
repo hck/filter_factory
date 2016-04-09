07518e4d1d68c7e2e9babde3bb6f46c7fd1d17aa
hck <alexsvirin@gmail.com>
2016-04-09 23:46:26 +0200

    Minor code reformatting.

--- lib/filter_factory.rb
+++ lib/filter_factory.rb
@@ -1,17 +1,17 @@
-require "filter_factory/version"
+require 'filter_factory/version'
 
-require "filter_factory/condition"
-require "filter_factory/field"
-require "filter_factory/filter"
+require 'filter_factory/condition'
+require 'filter_factory/field'
+require 'filter_factory/filter'
 
-require "filter_factory/active_record/condition"
-require "filter_factory/active_record/filter"
+require 'filter_factory/active_record/condition'
+require 'filter_factory/active_record/filter'
 
-require "filter_factory/mongoid/condition"
-require "filter_factory/mongoid/filter"
+require 'filter_factory/mongoid/condition'
+require 'filter_factory/mongoid/filter'
 
 module FilterFactory
-  def self.create &block
-    FilterFactory::Filter.create &block
+  def self.create(&block)
+    FilterFactory::Filter.create(&block)
   end
 end
--- lib/filter_factory/filter.rb
+++ lib/filter_factory/filter.rb
@@ -7,14 +7,14 @@ module FilterFactory
 
     attr_reader :fields
 
-    CONDITIONS = [:eq, :ne, :lt, :lte, :gt, :gte, :all, :in, :nin, :regex, :exists, :presents]
+    CONDITIONS = [:eq, :ne, :lt, :lte, :gt, :gte, :all, :in, :nin, :regex, :exists, :presents].freeze
 
     def initialize
       @fields = []
     end
 
     def attributes
-      @fields.inject(HashWithIndifferentAccess.new) do |acc,field|
+      @fields.inject(HashWithIndifferentAccess.new) do |acc, field|
         acc[field.alias] = field.value
         acc
       end
@@ -28,11 +28,11 @@ module FilterFactory
     end
 
     def filled_fields
-      fields.select{|f| !f.value.nil? && f.value != ''}
+      fields.select { |f| !f.value.nil? && f.value != '' }
     end
 
     def get_field(name)
-      fields.find{|f| f.name == name}
+      fields.find { |f| f.name == name }
     end
 
     def persisted?
@@ -40,29 +40,31 @@ module FilterFactory
     end
 
     CONDITIONS.each do |condition|
-      define_method condition do |name,options={}|
+      define_method condition do |name, options={}|
         field(name, condition, options)
-      end
-    end
-
-    private
-    def field(name, condition, options={})
-      Field.new(name, condition, options).tap do |field|
-        raise DuplicateFieldError if fields.include?(field)
-
-        define_singleton_method(field.alias){ field.value }
-        define_singleton_method("#{field.alias}="){|val| field.value = val }
-
-        @fields << field
       end
     end
 
     class << self
       def create(&block)
-        new.tap{|filter| filter.instance_eval &block}
+        new.tap { |filter| filter.instance_eval &block }
       end
     end
 
-    class DuplicateFieldError < StandardError; end
+    class DuplicateFieldError < StandardError;
+    end
+
+    private
+
+    def field(name, condition, options={})
+      Field.new(name, condition, options).tap do |field|
+        raise DuplicateFieldError if fields.include?(field)
+
+        define_singleton_method(field.alias) { field.value }
+        define_singleton_method("#{field.alias}=") { |val| field.value = val }
+
+        @fields << field
+      end
+    end
   end
 end

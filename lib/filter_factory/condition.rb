d982e1930de38338a99f12ef4c44b0e1d0de75ef
hck <alexsvirin@gmail.com>
2016-04-12 22:32:31 +0200

    Add RDoc/Yard comments to Condition class.

--- lib/filter_factory/condition.rb
+++ lib/filter_factory/condition.rb
@@ -2,6 +2,10 @@ module FilterFactory
   class Condition
     attr_reader :field_name, :value
 
+    # Initializes new instance of Condition class.
+    #
+    # @param [Symbol] field_name field name which will be used in condition
+    # @param [Object] value value which will be used in condition
     def initialize(field_name, value)
       @field_name, @value = field_name, value
     end

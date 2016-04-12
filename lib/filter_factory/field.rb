ed0be2ac2528197f8537fc4493cee2104455d8e3
hck <alexsvirin@gmail.com>
2016-04-12 22:33:08 +0200

    Add RDoc/Yard comments and minor style fixed for Field class.

--- lib/filter_factory/field.rb
+++ lib/filter_factory/field.rb
@@ -3,17 +3,29 @@ module FilterFactory
     attr_reader :name, :condition, :alias, :options
     attr_accessor :value
 
+    # Initializes new instance of Field.
+    #
+    # @param [Symbol, String] name
+    # @param [Symbol] condition
+    # @param [Hash] options
+    # @option options [Symbol, String] :alias field alias
     def initialize(name, condition, options = {})
       fail ArgumentError unless FilterFactory::Filter::CONDITIONS.include?(condition)
 
       valid_options = [:alias]
-      @name, @condition, @options = name, condition, options.reject{|k,| !valid_options.include?(k)}
+      @name, @condition, @options = name, condition, options.reject { |k,| !valid_options.include?(k) }
       @alias = @options[:alias] || @name
     end
 
+    # Checks whether two objects are equal.
+    # Returns true if obj is of the same class and has name,
+    # condition and alias attributes equal to current object.
+    #
+    # @param [Object] obj object to compare with
+    # @return [Boolean]
     def ==(obj)
       return false unless obj.is_a?(self.class)
-      [:name, :condition, :alias].inject(true) do |acc,attr|
+      [:name, :condition, :alias].inject(true) do |acc, attr|
         acc && public_send(attr) == obj.public_send(attr)
       end
     end

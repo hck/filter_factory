e918b2475f6610782b3740d204f68e8d42211648
hck <alexsvirin@gmail.com>
2016-04-14 11:23:20 +0200

    Explicitly add abstract condition methods to Condition class.

--- lib/filter_factory/condition.rb
+++ lib/filter_factory/condition.rb
@@ -9,5 +9,53 @@ module FilterFactory
     def initialize(field_name, value)
       @field_name, @value = field_name, value
     end
+
+    def eq(_obj)
+      fail NotImplementedError
+    end
+
+    def ne(_obj)
+      fail NotImplementedError
+    end
+
+    def lt(_obj)
+      fail NotImplementedError
+    end
+
+    def lte(_obj)
+      fail NotImplementedError
+    end
+
+    def gt(_obj)
+      fail NotImplementedError
+    end
+
+    def gte(_obj)
+      fail NotImplementedError
+    end
+
+    def all(_obj)
+      fail NotImplementedError
+    end
+
+    def is_in(_obj)
+      fail NotImplementedError
+    end
+
+    def not_in(_obj)
+      fail NotImplementedError
+    end
+
+    def regex(_obj)
+      fail NotImplementedError
+    end
+
+    def exists(_obj)
+      fail NotImplementedError
+    end
+
+    def presents(_obj)
+      fail NotImplementedError
+    end
   end
 end
